30195d55c8fd34aeaf9506ecee455f26481787f8
EvanEmi <159860526+EvanEmi@users.noreply.github.com>
2024-02-12 20:44:36 -0600

    Update codigos.c

--- codigos.c
+++ codigos.c
@@ -6,6 +6,10 @@ int main()
   int a,b,fact=1;
   printf("Escribe el número al que le quieres obtener su factorial:\n");
   scanf("%d",&a);
+  if(a<0){
+    printf("No se puede calcular el factorial de los numeros negativos");
+    return 0;
+  } else{
   for(b=a; b>1; b--)
   {
       fact=fact*b;
@@ -13,4 +17,5 @@ int main()
   printf("El factorial de %d = %d \n", a, fact);
   getchar();
   return 0;
+  }
 }
